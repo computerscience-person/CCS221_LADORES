175a3fba320fd4930eec3690e939acc4e452b0f1
Oliver <oliver.ladores@wvsu.edu.ph>
2023-03-10 21:59:33 +0800

    Set a variable in Exercises.TASK_2 module to private.

--- Exercises/TASK_2.py
+++ Exercises/TASK_2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-arr_2d = np.array([[1, 0, 1]
+_arr_2d = np.array([[1, 0, 1]
                    , [0, 0, 0]
                    , [1, 0, 1]])
 
@@ -20,11 +20,11 @@ def change(x, y, color):
     '''
     
     fig = plt.figure()
-    for i in range(len(arr_2d)):
-        for j in range(len(arr_2d)):
-            arr_2d[x][y] = color
+    for i in range(len(_arr_2d)):
+        for j in range(len(_arr_2d)):
+            _arr_2d[x][y] = color
 
-    img = plt.imshow(arr_2d, cmap='rainbow', interpolation='none')
+    img = plt.imshow(_arr_2d, cmap='rainbow', interpolation='none')
     img.set_clim([0, 100])
     plt.colorbar()
     

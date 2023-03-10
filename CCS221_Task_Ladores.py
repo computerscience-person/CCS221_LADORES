733b8fed312a78a601ac2c4786a8bfac3117b7b7
Oliver <oliver.ladores@wvsu.edu.ph>
2023-03-10 21:42:31 +0800

    Started implementation of TASK_3 to streamlit app.

--- CCS221_Task_Ladores.py
+++ CCS221_Task_Ladores.py
@@ -3,26 +3,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 import Exercises.TASK_1 as task1
 import Exercises.TASK_2 as task2
+import Exercises.TASK_3 as task3
 
 def main():
     st.title("Midterm Exam in CCS221")
     
     st.sidebar.header("Line Algorithm Parameters")
-    x0, y0, x1, y1 = st.sidebar.slider('Starting X', 1, 1000), \
+    _task1_x0, _task1_y0, _task1_x1, _task1_y1 = st.sidebar.slider('Starting X', 1, 1000), \
                      st.sidebar.slider('Starting Y', 1, 1000), \
                      st.sidebar.slider('Ending X', 1, 1000, 100), \
                      st.sidebar.slider('Ending Y', 1, 1000, 100)
+
+    st.sidebar.header("Change Pixel Hue Parameters")
     _task2_x, _task2_y, _task2_hue = st.sidebar.slider('X coordinate', 0, 2, 0, 1), \
                                      st.sidebar.slider('Y coordinate', 0, 2, 0, 1), \
                                      st.sidebar.slider('Hue', 0, 100, 50)
+                                    
+    st.sidebar.header("Image transformations")
+    _task3_filepath = st.sidebar.text_input('Enter file path: ')
     
     st.header("Task 1")
     st.subheader("DDA Line Algorithm")
-    st.pyplot(task1.DDALine(x0, y0, x1, y1, 'ro'))
+    st.pyplot(task1.DDALine(_task1_x0, _task1_y0, _task1_x1, _task1_y1, 'ro'))
     st.subheader("Bresenham's Line Algorithm")
-    st.pyplot(task1.Bres(x0, y0, x1, y1, 'ro'))
+    st.pyplot(task1.Bres(_task1_x0, _task1_y0, _task1_x1, _task1_y1, 'ro'))
     st.subheader("Midpoint Line Algorithm")
-    st.pyplot(task1.midpoint(x0, y0, x1, y1, 'ro'))
+    st.pyplot(task1.midpoint(_task1_x0, _task1_y0, _task1_x1, _task1_y1, 'ro'))
     
     st.header("Task 2")
     st.subheader("Change a pixel's color")

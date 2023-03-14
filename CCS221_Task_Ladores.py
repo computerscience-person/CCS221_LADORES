f8aa24aa8bacb23255d4762f5bc58e27773509d7
Oliver <oliver.ladores@wvsu.edu.ph>
2023-03-14 15:06:26 +0800

    Changed the implementation for image input in streamlit app.

--- CCS221_Task_Ladores.py
+++ CCS221_Task_Ladores.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
+from PIL import Image
 import cv2
 import Exercises.TASK_1 as task1
 import Exercises.TASK_2 as task2
@@ -21,7 +22,8 @@ def main():
                                      st.sidebar.slider('Hue', 0, 100, 50)
                                     
     st.sidebar.header("Image transformations")
-    _task3_filepath = st.sidebar.text_input('Enter file path: ', 'Exercises/images/img1.webp')
+    # _task3_filepath = st.sidebar.text_input('Enter file path: ', 'Exercises/images/img1.webp')
+    _task3_images = st.sidebar.file_uploader('Upload your files here', ['png', 'jpg', 'webp'], True)
     _task3_transformations = st.sidebar.multiselect('Select tranformations to apply: ', \
                             ['translate', 'rotate', 'reflect', 'scale', 'shear'])
 
@@ -64,28 +66,31 @@ def main():
     
     st.header("Task 3")
     st.subheader("Image Transformations")
-    st.write('File Path: ', _task3_filepath)
-    task3_image = cv2.cvtColor(cv2.imread(_task3_filepath), cv2.COLOR_BGR2RGB)
-    st.write('Original Image:')
-    st.pyplot(task3.visualize(task3_image))
-    st.write('Image Transformations: ', *_task3_transformations)
-    for transformation in _task3_transformations:
-        match transformation:
-            case 'translate':
-                st.write("Translation")
-                st.pyplot(task3.visualize(task3.translate(task3_image, _task3_translationx, _task3_translationy)))
-            case 'rotate':
-                st.write("Rotation")
-                st.pyplot(task3.visualize(task3.rotate(task3_image, _task3_rotation)))
-            case 'reflect':
-                st.write("Reflect")
-                st.pyplot(task3.visualize(task3.reflect(task3_image, _task3_reflection)))
-            case 'scale':
-                st.write("Scale")
-                st.pyplot(task3.visualize(task3.scale(task3_image, _task3_scale)))
-            case 'shear':
-                st.write("Shear")
-                st.pyplot(task3.visualize(task3.shear(task3_image, _task3_shearx, _task3_sheary)))
+    # st.write('File Path: ', _task3_filepath)
+    for task3_image in _task3_images:
+        task3_image = Image.open(task3_image)
+        # task3_image = cv2.cvtColor(np.asarray(task3_image), cv2.COLOR_BGR2RGB)
+        task3_image = np.asarray(task3_image)
+        st.write('Original Image:')
+        st.pyplot(task3.visualize(task3_image))
+        st.write('Image Transformations: ', *_task3_transformations)
+        for transformation in _task3_transformations:
+            match transformation:
+                case 'translate':
+                    st.write("Translation")
+                    st.pyplot(task3.visualize(task3.translate(task3_image, _task3_translationx, _task3_translationy)))
+                case 'rotate':
+                    st.write("Rotation")
+                    st.pyplot(task3.visualize(task3.rotate(task3_image, _task3_rotation)))
+                case 'reflect':
+                    st.write("Reflect")
+                    st.pyplot(task3.visualize(task3.reflect(task3_image, _task3_reflection)))
+                case 'scale':
+                    st.write("Scale")
+                    st.pyplot(task3.visualize(task3.scale(task3_image, _task3_scale)))
+                case 'shear':
+                    st.write("Shear")
+                    st.pyplot(task3.visualize(task3.shear(task3_image, _task3_shearx, _task3_sheary)))
     
     if st.button("Exit"):
         st.stop()
